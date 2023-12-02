2aca7cf69f88420c30a064f4f46b534fba344f27
Wilbur Zhang <wilbur.zhaang@gmail.com>
2023-12-02 12:56:37 -0500

    markdown and include support

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import data from "@/../data.json";
+import Markdown from "react-markdown";
 
 const Experience = () => {
   return (
@@ -8,37 +9,41 @@ const Experience = () => {
         experience
       </h1>
 
-      {data.experience.map((experience, index) => (
-        <Fragment key={index}>
-          <div
-            key={index}
-            className="flex flex-col col-span-6 font-medium -mt-3"
-          >
-            <div className="flex space-x-2">
-              <h2 className="text-[0.875rem] font-sans font-semibold">
-                {experience.organization}
-              </h2>
-              <p className="ml-2 font-sans text-[0.875rem] font-medium">
-                {experience.position}
-              </p>
-              <span className="grow"></span>
-              <p className="font-sans text-[0.875rem] font-medium text-dawn-subtle">
-                {experience.duration}
-              </p>
+      {data.experience
+        .filter((experience) => experience.include)
+        .map((experience, index) => (
+          <Fragment key={index}>
+            <div
+              key={index}
+              className="flex flex-col col-span-6 font-medium -mt-3"
+            >
+              <div className="flex space-x-2">
+                <h2 className="text-[0.875rem] font-sans font-semibold">
+                  {experience.organization}
+                </h2>
+                <p className="ml-2 font-sans text-[0.875rem] font-medium">
+                  {experience.position}
+                </p>
+                <span className="grow"></span>
+                <p className="font-sans text-[0.875rem] font-medium text-dawn-subtle">
+                  {experience.duration}
+                </p>
+              </div>
+              <span className="text-[0.75rem] font-mono font-medium text-dawn-subtle italic mt-0.5">
+                {experience.technologies}
+              </span>
             </div>
-            <span className="text-[0.75rem] font-mono font-medium text-dawn-subtle italic mt-0.5">
-              {experience.technologies}
-            </span>
-          </div>
-          <div className="flex flex-col col-span-6 font-sans text-[0.875rem] ml-4 -mt-3.5">
-            <ul>
-              {experience.responsibilities.map((li) => (
-                <li key={li.length}>{li}</li>
-              ))}
-            </ul>
-          </div>
-        </Fragment>
-      ))}
+            <div className="flex flex-col col-span-6 font-sans text-[0.875rem] ml-4 -mt-3.5">
+              <ul>
+                {experience.responsibilities.map((li, index) => (
+                  <li key={index}>
+                    <Markdown>{li}</Markdown>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Fragment>
+        ))}
     </>
   );
 };
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,6 @@
 import data from "@/../data.json";
 import { Fragment } from "react";
+import Markdown from "react-markdown";
 
 const Projects = () => {
   return (
@@ -8,32 +9,36 @@ const Projects = () => {
         projects
       </h1>
 
-      {data.projects.map((project, index) => (
-        <Fragment key={index}>
-          <div className="flex flex-col col-span-6 font-semibold">
-            <div className="flex space-x-2 -mt-3">
-              <a href={project.link} target="_blank">
-                <h2 className="text-[0.875rem] font-sans font-semibold">
-                  {project.name}
-                </h2>
-              </a>
-              <p className="font-sans text-[0.875rem] font-medium">
-                {project.description}
-              </p>
+      {data.projects
+        .filter((project) => project.include)
+        .map((project, index) => (
+          <Fragment key={index}>
+            <div className="flex flex-col col-span-6 font-semibold">
+              <div className="flex space-x-2 -mt-3">
+                <a href={project.link} target="_blank">
+                  <h2 className="text-[0.875rem] font-sans font-semibold">
+                    {project.name}
+                  </h2>
+                </a>
+                <p className="font-sans text-[0.875rem] font-medium">
+                  {project.description}
+                </p>
+              </div>
+              <span className="text-[0.75rem] font-mono font-medium text-dawn-subtle italic mt-0.5">
+                {project.technologies}
+              </span>
             </div>
-            <span className="text-[0.75rem] font-mono font-medium text-dawn-subtle italic mt-0.5">
-              {project.technologies}
-            </span>
-          </div>
-          <div className="flex flex-col col-span-6 font-sans text-[0.875rem] ml-4 -mt-3.5">
-            <ul>
-              {project.highlights.map((li) => (
-                <li key={li.length}>{li}</li>
-              ))}
-            </ul>
-          </div>
-        </Fragment>
-      ))}
+            <div className="flex flex-col col-span-6 font-sans text-[0.875rem] ml-4 -mt-3.5">
+              <ul>
+                {project.highlights.map((li, index) => (
+                  <li key={index}>
+                    <Markdown>{li}</Markdown>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Fragment>
+        ))}
     </>
   );
 };
